9d5a6dbb41b50db2ee970fefeb3d85c32ea9d299
Robin <144045530+WeirdAl62@users.noreply.github.com>
2026-04-27 21:00:37 -0400

    Add interrupt handling to gray_display.py

--- gray_display.py
+++ gray_display.py
@@ -12,6 +12,8 @@ def main():
         print('Usage:')
         print('python gray_display.py [filename] [delay between lines in seconds]')
         print('Filenames outputted by gray.py look like "gray_[length in bits].txt"')
+    except KeyboardInterrupt:
+        print('\nSee you later!')
 
 if __name__ == '__main__':
     main()
