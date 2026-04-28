0ae89d708b4f3e4abd1b719837820dd3d34c9003
Robin <144045530+WeirdAl62@users.noreply.github.com>
2026-04-27 21:09:32 -0400

    Change error message to just show usage in gray_display.py

--- gray_display.py
+++ gray_display.py
@@ -8,7 +8,6 @@ def main():
                 print(line, end='')
                 time.sleep(float(sys.argv[2]))
     except (IndexError, FileNotFoundError):
-        print(sys.argv[1] + ' is not a file!')
         print('Usage:')
         print('python gray_display.py [filename] [delay between lines in seconds]')
         print('Filenames outputted by gray.py look like "gray_[length in bits].txt"')
